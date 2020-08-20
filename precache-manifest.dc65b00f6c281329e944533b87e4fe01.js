3b3ce19abbf6ef608ecaf3c24ee8a6cdb77ff016
krissdrawing <krx95@o2.pl>
2020-08-20 11:01:46 +0200

    Updates

---
+++ precache-manifest.dc65b00f6c281329e944533b87e4fe01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93d10797d5e0fb3b3699",
+    "url": "/golden-rule/static/css/main.eb90fcc9.chunk.css"
+  },
+  {
+    "revision": "93d10797d5e0fb3b3699",
+    "url": "/golden-rule/static/js/main.1d89a5ca.chunk.js"
+  },
+  {
+    "revision": "ee8b5b863670bfbc3f3c",
+    "url": "/golden-rule/static/js/runtime~main.2544b02c.js"
+  },
+  {
+    "revision": "c946403b6df739f6ceae",
+    "url": "/golden-rule/static/js/2.c5d67210.chunk.js"
+  },
+  {
+    "revision": "6876dfd606c703fe6bc894b2cce229b2",
+    "url": "/golden-rule/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/golden-rule/precache-manifest.48c04803f853438277d0fc4131d0a27d.js"
+  "/golden-rule/precache-manifest.dc65b00f6c281329e944533b87e4fe01.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/golden-rule/index.html"), {
+workbox.routing.registerNavigationRoute("/golden-rule/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
